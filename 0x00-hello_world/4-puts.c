f88ff9d36f6ba4722c31c4736bbaa40146d728b6
dakinemoarani <dnemaorani074@gmail.com>
2022-06-02 10:01:54 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
-prints a string using puts function
-Return 0 if success
-*/
+ *prints a string using puts function
+ *Return 0 if success
+ */
 
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
 
